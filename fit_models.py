b70a2ee8f639fd83e9437998278d6d81ae5a1e97
Teddy Groves <groves.teddy@gmail.com>
2021-04-28 23:07:08 +0200

    Update fitting script

--- fit_models.py
+++ fit_models.py
@@ -1,10 +1,11 @@
-from munging import prepare_data
+import os
+
+import arviz as az
 import pandas as pd
 from cmdstanpy import CmdStanModel
-from cmdstanpy.utils import jsondump, get_logger
-import arviz as az
-import numpy as np
-import os
+from cmdstanpy.utils import get_logger, jsondump
+
+from munging import prepare_data
 from util import get_99_pct_params_ln
 
 PRIORS = {
@@ -13,8 +14,16 @@ PRIORS = {
     "prior_td": get_99_pct_params_ln(0.4, 7.5),
     "prior_kd": get_99_pct_params_ln(0.05, 2.5),
     "prior_R0": get_99_pct_params_ln(2, 3),
-    "prior_err": get_99_pct_params_ln(0.05, 0.13)
+    "prior_err": get_99_pct_params_ln(0.05, 0.13),
 }
+CSV_FILE = os.path.join("raw_data", "AllFlaskData_compiled.csv")
+LIKELIHOOD = 1
+OUTPUT_DIR = "results"
+LOO_DIR = os.path.join(OUTPUT_DIR, "loo")
+INFD_DIR = os.path.join(OUTPUT_DIR, "infd")
+SAMPLES_DIR = os.path.join(OUTPUT_DIR, "samples")
+LIKELIHOOD = True
+
 SAMPLE_CONFIG = dict(
     show_progress=False,
     save_warmup=False,
@@ -22,15 +31,9 @@ SAMPLE_CONFIG = dict(
     iter_warmup=600,
     iter_sampling=600,
     chains=4,
-    seed=12345
+    seed=12345,
+    output_dir=SAMPLES_DIR,
 )
-CSV_FILE = os.path.join("raw_data", "AllFlaskData_compiled.csv")
-LIKELIHOOD = 1
-OUTPUT_DIR = "results"
-LOO_DIR = os.path.join(OUTPUT_DIR, "loo")
-INFD_DIR = os.path.join(OUTPUT_DIR, "infd")
-LIKELIHOOD = True
-
 
 TREATMENT_SET_1 = {
     "puromycin": "15ug/mL Puromycin",
@@ -42,41 +45,42 @@ TREATMENT_SET_2 = {
 }
 X_COL_1 = {
     "ab": ["is_A", "is_B", "is_AB"],
-    "abc": ["is_A", "is_B", "is_AB", "is_C", "is_AC", "is_BC", "is_ABC"]
+    "abc": ["is_A", "is_B", "is_AB", "is_C", "is_AC", "is_BC", "is_ABC"],
 }
-X_COL_2 = {
-    "ab": ["is_AB"],
-    "abc": ["is_AB", "is_C", "is_ABC"]
-}
+X_COL_2 = {"ab": ["is_AB"], "abc": ["is_AB", "is_C", "is_ABC"]}
 STAN_FILES = {
-    "m1":  "model_kq_design_effects.stan",
-    "m2":  "model_no_kq_design_effects.stan",
+    "m1": "model_kq_design_effects.stan",
+    "m2": "model_no_kq_design_effects.stan",
 }
 TREATMENT_SETS = {
-    'Treatment_1' : (TREATMENT_SET_1, X_COL_1),
-    'Treatment_2' : (TREATMENT_SET_2, X_COL_2)
+    "Treatment_1": (TREATMENT_SET_1, X_COL_1),
+    "Treatment_2": (TREATMENT_SET_2, X_COL_2),
 }
+
 
 def get_stan_input(msmts, priors, x_clone_cols):
     x_clone = msmts.groupby("clone_fct")[x_clone_cols].first().astype(int)
-    return {**priors, **{
-        "N": len(msmts),
-        "N_test": len(msmts),
-        "R": msmts["replicate"].nunique(),
-        "C": msmts["clone"].nunique(),
-        "D": msmts["design"].nunique(),
-        "K": x_clone.shape[1],
-        "design": msmts.groupby("clone_fct")["design_fct"].first().values,
-        "clone": msmts.groupby("replicate_fct")["clone_fct"].first().values,
-        "x_clone": x_clone.values,
-        "replicate": msmts["replicate_fct"].values,
-        "t": msmts["day"].values,
-        "y": msmts["y"].values,
-        "replicate_test": msmts["replicate_fct"].values,
-        "t_test": msmts["day"].values,
-        "y_test": msmts["y"].values,
-        "likelihood": int(LIKELIHOOD),
-    }}
+    return {
+        **priors,
+        **{
+            "N": len(msmts),
+            "N_test": len(msmts),
+            "R": msmts["replicate"].nunique(),
+            "C": msmts["clone"].nunique(),
+            "D": msmts["design"].nunique(),
+            "K": x_clone.shape[1],
+            "design": msmts.groupby("clone_fct")["design_fct"].first().values,
+            "clone": msmts.groupby("replicate_fct")["clone_fct"].first().values,
+            "x_clone": x_clone.values,
+            "replicate": msmts["replicate_fct"].values,
+            "t": msmts["day"].values,
+            "y": msmts["y"].values,
+            "replicate_test": msmts["replicate_fct"].values,
+            "t_test": msmts["day"].values,
+            "y_test": msmts["y"].values,
+            "likelihood": int(LIKELIHOOD),
+        },
+    }
 
 
 def get_infd_kwargs(msmts, x_cols):
@@ -88,7 +92,7 @@ def get_infd_kwargs(msmts, x_cols):
             "design": x_cols,
             "clone": msmts.groupby("clone_fct")["clone"].first(),
             "replicate": msmts.groupby("replicate_fct")["replicate"].first(),
-            "cv_effects": ["q", "tau", "d"]
+            "cv_effects": ["q", "tau", "d"],
         },
         dims={
             "R0": ["replicate"],
@@ -100,8 +104,9 @@ def get_infd_kwargs(msmts, x_cols):
             "sd_cv": ["cv_effects"],
             "llik": ["replicate"],
         },
-        save_warmup=SAMPLE_CONFIG["save_warmup"]
+        save_warmup=SAMPLE_CONFIG["save_warmup"],
     )
+
 
 def main():
     logger = get_logger()
@@ -115,9 +120,7 @@ def main():
                     run_name = f"{treatment_label}_{model_name}_{xname}"
                     loo_file = os.path.join(LOO_DIR, f"loo_{run_name}.pkl")
                     infd_file = os.path.join(INFD_DIR, f"infd_{run_name}.ncdf")
-                    json_file = os.path.join(
-                        OUTPUT_DIR, f"input_data_{run_name}.json"
-                    )
+                    json_file = os.path.join(OUTPUT_DIR, f"input_data_{run_name}.json")
                     print(f"Fitting model {run_name}...")
                     model = CmdStanModel(stan_file=stan_file, logger=logger)
                     msmts = prepare_data(pd.read_csv(CSV_FILE), treatment=treatment)
@@ -139,7 +142,7 @@ def main():
             comparison.to_csv(
                 os.path.join(LOO_DIR, f"loo_comparison_{treatment_label}.csv")
             )
-            
+
 
 if __name__ == "__main__":
     main()
